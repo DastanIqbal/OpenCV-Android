ae6eb8ca47b1234fcf26bc7baae377e5bfd9b3e0
Iqbal Ahmed <ask2iqbal@gmail.com>
2022-01-23 23:27:37 +0400

    Add files via upload

---
+++ android_env.sh
@@ -0,0 +1,8 @@
+export NDK=/opt/android-sdk/ndk/
+
+export ANDROID_SDK=/opt/android-sdk
+export ANDROID_SDK_TOOLS=$ANDROID_SDK/tools/bin
+export ANDROID_SDK_PLATFORM=$ANDROID_SDK/platform-tools
+export ANDROID_SDK_ROOT=ANDROID_SDK
+
+export PATH=$PATH:$ANDROID_SDK_TOOLS:$ANDROID_SDK_PLATFORM:$NDK
